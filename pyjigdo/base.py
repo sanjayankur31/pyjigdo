239b44d301a348ad5696814d14b93915d83b74aa
Jonathan Steffan (daMaestro) <jonathansteffan@gmail.com>
2009-02-14 17:18:33 -0700

    Update copyleft dates. Yes, we are still here.

--- pyjigdo/base.py
+++ pyjigdo/base.py
@@ -1,5 +1,5 @@
 #
-# Copyright 2007, 2008 Fedora Unity Project (http://fedoraunity.org)
+# Copyright 2007-2009 Fedora Unity Project (http://fedoraunity.org)
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
