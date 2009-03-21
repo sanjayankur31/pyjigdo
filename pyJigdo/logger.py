4178f386569d58ed02b802fffdfe7a8e8bd3c544
Jonathan Steffan (daMaestro) <jonathansteffan@gmail.com>
2009-03-21 16:18:37 -0600

    Fix up the logger. Make it more generic and support all log levels. Remove the debug level argument as we are going to move to using the logging module as it's designed to be used.

--- pyJigdo/logger.py
+++ pyJigdo/logger.py
@@ -15,131 +15,49 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 
-import logging
-import logging.handlers
-import sys
+import logging, sys
 
-import pyJigdo.translate as translate
-from pyJigdo.translate import _, N_
+class pyJigdoLogger:
+    def __init__(self, logfile, loglevel=logging.WARNING, file_loglevel=logging.INFO):
+        """ pyJigdoLogger is used to log to the interface and to a file.
+            Default is to prefer logging more data to a file and less to
+            to console. """
 
-class Logger:
-    def __init__(self, base, loglevel = logging.INFO, debuglevel = 0):
-        """ Our main way to log data and report errors. """
         self.loglevel = loglevel
-        self.debuglevel = debuglevel
-        self.base = base
+        self.file_loglevel = file_loglevel
 
         console_logging = logging.Formatter("\n%(message)s\n")
-        file_logging = logging.Formatter("%(message)s")
+        file_logging = logging.Formatter("%(asctime)s - %(message)s")
 
         console_stdout = logging.StreamHandler(sys.stdout)
         console_stdout.setFormatter(console_logging)
+        console_stdout.setLevel(self.loglevel)
 
-        filelog_handler = logging.FileHandler(filename = "pyjigdo.log")
+        filelog_handler = logging.FileHandler(filename = logfile)
         filelog_handler.setFormatter(file_logging)
+        filelog_handler.setLevel(self.file_loglevel)
 
         self.log = logging.getLogger()
         self.log.addHandler(console_stdout)
         self.log.addHandler(filelog_handler)
 
-        self.log.setLevel(self.loglevel)
-
-    def set_config(self, cfg):
-        """ Let the Logger instance know what our configuration is and she might
-        be able to distinct between CLI and GUI mode, or even give more details
-        about what goes wrong. """
-        self.cfg = cfg
-    
-    def status(self, msg):
-        """ Send a status. If len() > 79, data will be truncated. """
-        if len(msg) > 79: msg = "%s..." % msg[:76]
-        print '%s%s\r' % (msg, " "*(79 - len(str(msg)))),
-        sys.stdout.flush()
+    def debug(self, msg):
+        """ Log a debug level event. """
+        self.log.debug(msg)
 
     def info(self, msg):
+        """ Log an info level event. """
         self.log.info(msg)
 
-    def debug(self, msg, level = 1):
-        # By default, level = 1 so that debug messages are suppressed
-        if level <= self.debuglevel:
-            self.log.debug(msg)
-
-    def error(self, msg, recoverable = True):
-        self.log.error(msg)
-        # FIXME: No GUI mode
-        #if self.cfg.gui_mode:
-        #    self.error_box(msg)
-        #elif self.cfg.cli_mode:
-        if self.cfg.cli_mode:
-            if recoverable:
-                self.error_prompt(msg)
-            else:
-                self.base.abort()
-
     def warning(self, msg):
+        """ Log a warning level event. """
         self.log.warning(msg)
-        # FIXME: No GUI mode
-        #if self.cfg.gui_mode:
-        #    self.warning_box(msg)
-        #elif self.cfg.cli_mode:
-        if self.cfg.cli_mode:
-            self.warning_prompt(msg)
 
-    def error_box(self, text):
-        """Display an Error Message Box"""
-        import gtk
-        dlg = gtk.MessageDialog(None, 0, gtk.MESSAGE_ERROR, gtk.BUTTONS_OK, text)
-        dlg.set_title(_("Error"))
-        dlg.set_default_size(100, 100)
-        dlg.set_position (gtk.WIN_POS_CENTER)
-        dlg.set_border_width(2)
-        dlg.set_modal(True)
-        dlg.run()
-        dlg.hide()
-        dlg.destroy()
-        self._runGtkMain()
+    def error(self, msg):
+        """ Log an error level event. """
+        self.log.error(msg)
 
-    def warning_box(self, text):
-        """Display an Error Message Box"""
-        import gtk
-        dlg = gtk.MessageDialog(None, 0, gtk.MESSAGE_WARNING, gtk.BUTTONS_OK, text)
-        dlg.set_title(_("Warning"))
-        dlg.set_default_size(100, 100)
-        dlg.set_position (gtk.WIN_POS_CENTER)
-        dlg.set_border_width(2)
-        dlg.set_modal(True)
-        dlg.run()
-        dlg.hide()
-        dlg.destroy()
-        self._runGtkMain()
+    def critical(self, msg):
+        """ Log a critical level event. """
+        self.log.critical(msg)
 
-    def error_prompt(self, text):
-        """ The error has already been logged to the console, ask the user what they
-            would like to do now. """
-        sys.stderr.write(_("Do you want to continue? [Y/n]") + " ")
-        try:
-            answer = sys.stdin.readline()[:-1]
-            if answer.lower() in ("n", "no"):
-               self.error(_("Abort due to user request."), recoverable = False)
-        except KeyboardInterrupt:
-            self.error(_("Abort due to user interupt."), recoverable = False)
-
-    def warning_prompt(self, text):
-        """ The error has already been logged to the console, ask the user what they
-            would like to do now. """
-        sys.stdout.write(_("Do you want to continue? [Y/n]") + " ")
-        try:
-            answer = sys.stdin.readline()[:-1]
-            if answer.lower() in ("n", "no"):
-                self.error(_("Abort due to user request."), recoverable = False)
-        except KeyboardInterrupt:
-            self.error(_("Abort due to user interupt."), recoverable = False)
-
-    # Master GTK Interface update routine
-    # This is a hack that should be replaced with twisted bindings for event driven
-    # user interface. We will try to design the upcoming download gears around
-    # conditional callbacks, for when we are in GUI mode or CLI mode.
-    def _runGtkMain(*args):
-        import gtk
-        while gtk.events_pending():
-            gtk.main_iteration()
